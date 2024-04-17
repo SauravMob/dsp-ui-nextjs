cacef4dc7c67ed7e8d966fbbb64829af87cb6cbb
Saurav193 <saurav.upadhyay@mobavenue.com>
2024-04-17 11:57:20 +0530

    Navbar added

---
+++ components/ThemeToggler.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+import { Moon, Sun } from 'lucide-react'
+import React, { useEffect, useState } from 'react'
+
+export default function ThemeToggler() {
+    const [darkMode, setDarkMode] = useState(false)
+
+    useEffect(() => {
+        const theme = localStorage.getItem('theme')
+        if (theme === "dark") setDarkMode(true)
+    }, [])
+
+    useEffect(() => {
+        if (darkMode) {
+            document.documentElement.classList.add("dark")
+            localStorage.setItem("theme", "dark")
+        } else {
+            document.documentElement.classList.remove("dark")
+            localStorage.setItem("theme", "light")
+        }
+    }, [darkMode])
+
+    return (
+        <div
+            className='relative w-16 h-8 flex items-center dark:bg-gray-700 bg-teal-500 cursor-pointer rounded-full p-1'
+            onClick={() => setDarkMode(!darkMode)}
+        >
+            <Moon className='text-white' size={18} />
+            <div
+                className='absolute bg-white w-6 h-6 rounded-full shadow-md transform transition-transform duration-300'
+                style={darkMode ? { left: "2px" } : { right: "2px" }}
+            ></div>
+            <Sun className='ml-auto text-yellow-400' size={18} />
+        </div>
+    )
+}
