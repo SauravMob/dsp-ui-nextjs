5e72720a1b91a1c826d1e3668b7192ee61ce9bc6
Saurav193 <saurav.upadhyay@mobavenue.com>
2024-04-20 19:07:40 +0530

    Custom Breadcrumb

--- app/(authenticated)/dashboard/page.tsx
+++ app/(authenticated)/dashboard/page.tsx
@@ -1,36 +1,30 @@
 import React from 'react'
-import {
-    Breadcrumb,
-    BreadcrumbItem,
-    BreadcrumbLink,
-    BreadcrumbList,
-    BreadcrumbPage,
-    BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb"
+
 import { LayoutDashboard } from 'lucide-react'
 import { Card } from '@/components/ui/card'
 
 export default function pages() {
     return (
         <div>
-            <Breadcrumb>
-                <BreadcrumbList>
-                    <BreadcrumbItem>
-                        <BreadcrumbLink href="/">Home</BreadcrumbLink>
-                    </BreadcrumbItem>
-                    <BreadcrumbSeparator />
-                    <BreadcrumbItem>
-                        <BreadcrumbLink href="/dashboard">Dashboard</BreadcrumbLink>
-                    </BreadcrumbItem>
-                </BreadcrumbList>
-            </Breadcrumb>
-
-            <div className='font-bold my-2 text-2xl flex items-center'>
-                <LayoutDashboard size={20} className='mr-1'/> Dashboard
+            <div className='font-bold mb-4 text-2xl flex items-center'>
+                <LayoutDashboard size={20} className='mr-1' /> Dashboard
             </div>
 
-            <Card className=' min-h-96'>
-                hdfhsdfj
+            <Card className='min-h-96 p-4'>
+                <div className='grid grid-cols-4 gap-2'>
+                    <div className='col-span-1 flex justify-center'>
+                        Card1
+                    </div>
+                    <div className='col-span-1 flex justify-center'>
+                        Card2
+                    </div>
+                    <div className='col-span-1 flex justify-center'>
+                        Card3
+                    </div>
+                    <div className='col-span-1 flex justify-center'>
+                        Card4
+                    </div>
+                </div>
             </Card>
         </div>
     )
---
+++ components/utility/CustomBreadCrumb.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb"
+
+export default function CustomBreadCrumb(
+    {
+        secondItem,
+        secondLink,
+        thirdItem,
+        thirdLink
+    }: {
+        secondItem: string,
+        secondLink: string,
+        thirdItem?: string,
+        thirdLink?: string
+    }
+) {
+    return (
+        <Breadcrumb>
+            <BreadcrumbList>
+                <BreadcrumbItem>
+                    <BreadcrumbLink href="/dashboard">Home</BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator />
+                <BreadcrumbItem>
+                    <BreadcrumbLink href={secondLink}>{secondItem}</BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbItem>
+                    <BreadcrumbLink href={thirdLink}>{thirdItem}</BreadcrumbLink>
+                </BreadcrumbItem>
+            </BreadcrumbList>
+        </Breadcrumb>
+    )
+}
